255bc4d224fc64260a51f255c560469859ad2c05
Vladislav-Martian <me.as.martian@gmail.com>
2021-04-01 19:11:44 +0300

    App start

---
+++ Recryptor/Options.cs
@@ -0,0 +1,74 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recryptor
+{
+    /// <summary>
+    /// <para>
+    /// Class represents options if program called to encrypt file by simplest way, saving its name and extension but with obfuscated binary data. Second call decrypts file back. File size do not changes.
+    /// </para>
+    /// </summary>
+    [Verb("xor", HelpText = "Transform byte representation of file by xor-encryption. " +
+        "If you use one key twice file will be encrypted end recrypted. Don`t forget key")]
+    class XorOptions
+    {
+        [Option('f', "file", Required = true, HelpText = "File adress. Example: c:\\temp\\MyTest.txt")]
+        public string FilePath { get; set; }
+
+        [Option('k', "key", Required = true, HelpText = "Key to encrypt file, used as UTF8 string.")]
+        public string Password { get; set; }
+
+        [Option('r', "rename", Required = false, Default = null,
+            HelpText = "Rename resulting file. By default original file name used.")]
+        public string Rename { get; set; }
+    }
+
+    /// <summary>
+    /// <para>
+    /// Class represents options if program called to advanced encryption of file. Obfuscate file name and extension. File size changes. Result extension ".crypto".
+    /// </para>
+    /// </summary>
+    [Verb("store", HelpText = "Transform file to its .crypto version. It practically unpossible" +
+        " to restore back file, file original name and extension without keyword.")]
+    class StoreOptions
+    {
+        [Option('f', "file", Required = true, HelpText = "File adreess. Example: c:\\temp\\file.bin")]
+        public string FilePath { get; set; }
+
+        [Option('k', "key", Required = true, HelpText = "Key to encrypt file, used as UTF8 string.")]
+        public string Password { get; set; }
+
+        [Option('r', "rename", Required = false, Default = null,
+            HelpText = "Rename resulting file. By default original file name used.")]
+        public string Rename { get; set; }
+
+        [Option('d', "delete", Required = false, Default = (bool) false,
+            HelpText = "Delete original file.")]
+        public bool Delete { get; set; }
+    }
+    /// <summary>
+    /// <para>
+    /// Class represents options if program called to restore stored file. Deobfuscate file name and extension. File size changes. Result extension ".crypto".
+    /// </para>
+    /// </summary>
+    [Verb("restore", HelpText = "Restore file from its .crypto version. It practically unpossible" +
+        " to restore back file data, file original name and extension without keyword.")]
+    class RestoreOptions
+    {
+        [Option('f', "file", Required = true, HelpText = "File adreess. Example: c:\\temp\\file.bin")]
+        public string FilePath { get; set; }
+
+        [Option('k', "key", Required = true, HelpText = "Key to encrypt file, used as UTF8 string.")]
+        public string Password { get; set; }
+
+        [Option('r', "rename", Required = false, Default = null,
+            HelpText = "Rename resulting file. By default original file name used.")]
+        public string Rename { get; set; }
+
+        [Option('d', "delete", Required = false, Default = (bool)false,
+            HelpText = "Delete stored file.")]
+        public bool Delete { get; set; }
+    }
+}
---
+++ Recryptor/Program.cs
@@ -0,0 +1,102 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Recrypt.Core;
+
+namespace Recryptor
+{
+    class Program
+    {
+        public const string Version = "1.0.0-Alpha";
+
+        public static void Main(string[] args)
+        {
+            var parser = new Parser(with => with.HelpWriter = null);
+            var parserResult = parser.ParseArguments<XorOptions, StoreOptions, RestoreOptions>(args);
+            parserResult
+            .WithParsed<XorOptions>(HandleXorCall)
+            .WithParsed<StoreOptions>(HandleStoreCall)
+            .WithParsed<RestoreOptions>(HandleRestoreCall)
+            .WithNotParsed(errs => DisplayHelp(parserResult, errs));
+        }
+
+        private static void HandleXorCall(XorOptions opts)
+        {
+            var finfo = new FileInfo(opts.FilePath);
+            if (!finfo.Exists)
+            {
+                throw new IOException("File doesn`t exists.");
+            }
+            using (var file = finfo.Open(FileMode.Open, FileAccess.ReadWrite))
+            {
+                CryptoEngine.Recrypt(file, Byter.GetBytes(opts.Password));
+            }
+            if (opts.Rename != null)
+            {
+                finfo.MoveTo(Path.Combine(finfo.DirectoryName, opts.Rename + finfo.Extension));
+            }
+        }
+
+        private static void HandleStoreCall(StoreOptions opts)
+        {
+            var finfo = new FileInfo(opts.FilePath);
+            if (!finfo.Exists)
+            {
+                throw new IOException("File doesn`t exists.");
+            }
+            var fm = FileModel.Scan(opts.FilePath);
+            if (opts.Rename != null)
+            {
+                fm = fm.EncryptAndPack(Byter.GetBytes(opts.Password), opts.Rename);
+            }
+            else
+            {
+                fm = fm.EncryptAndPack(Byter.GetBytes(opts.Password), fm.Name);
+            }
+            fm.Save(finfo.DirectoryName);
+            if (opts.Delete)
+            {
+                finfo.Delete();
+            }
+        }
+
+        private static void HandleRestoreCall(RestoreOptions opts)
+        {
+            var finfo = new FileInfo(opts.FilePath);
+            if (!finfo.Exists)
+            {
+                throw new IOException("File doesn`t exists.");
+            }
+            var fm = FileModel.Scan(opts.FilePath);
+            fm = fm.DecryptAndUnpack(Byter.GetBytes(opts.Password));
+            if (opts.Rename != null)
+            {
+                fm.Save(finfo.DirectoryName, opts.Rename);
+            }
+            else
+            {
+                fm.Save(finfo.DirectoryName, fm.Name);
+            }
+            if (opts.Delete)
+            {
+                finfo.Delete();
+            }
+        }
+
+        //==================================================//
+        // Help text
+        static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            var helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.AdditionalNewLineAfterOption = false;
+                h.Heading = "Recryptor " + Version;
+                h.Copyright = "Copyright (c) 2021 Recryptor";
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e);
+            Console.WriteLine(helpText);
+        }
+    }
+}
